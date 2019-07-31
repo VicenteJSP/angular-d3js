b45e48d00c78e54ed46444478a0ec224b214db7e
Vicente <VicenteJSP@outlook.com>
2019-07-30 21:20:27 -0500

    grafica de pastel

--- src/app/d3js/bar-chart/bar-chart.component.ts
+++ src/app/d3js/bar-chart/bar-chart.component.ts
@@ -1,4 +1,3 @@
-import { element } from 'protractor';
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import * as d3 from 'd3';
 
@@ -27,8 +26,37 @@ export class BarChartComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.svgBarChart(this.barChartSVG.nativeElement, this.data2);
-    this.cargarJSON('assets/data/barchart.json');
+    // this.svgBarChart(this.barChartSVG.nativeElement, this.data2);
+    this.cargarJSON('assets/data/datosPastel.json', this.barChart.nativeElement, this.svgPastel,
+    { h: window.innerHeight, w: window.innerWidth});
+  }
+
+  onResize(event) {
+    this.cargarJSON('assets/data/datosPastel.json', this.barChart.nativeElement, this.svgPastel,
+    { h: event.target.innerHeight, w: event.target.innerWidth});
+  }
+
+  private svgPastel(selection: any, data: Array<any>, sizeWindow: { h: any, w: any }) {
+    d3.select('svg').remove();
+    const width = sizeWindow.w - 20;
+    const height = 400;
+    const radius = Math.min(width, height) / 2;
+    const color = d3.scaleOrdinal()
+      .range(['LimeGreen', 'RoyalBlue', 'SlateBlue', 'BlueViolet', 'OrangeRed', 'Yellow', 'Brown', 'DarkOrchid']);
+    const arc = d3.arc().outerRadius(radius - 10).innerRadius(50);
+    const pie = d3.pie().value( (d: any) => d.dato);
+    const svg = d3.select(selection).append('svg').attr('width', width).attr('height',  height).style('border', '1px solid #aaa')
+    .append('g').attr('transform', `translate(${width / 2}, ${height / 2})`);
+    const g = svg.selectAll('.arc').data(pie(data)).enter().append('g').attr('class', 'arc');
+
+    g.append<SVGPathElement>('path').attr('d', arc).style('fill', (d: any) => color(d.data.nombre));
+    g.append('text').text((d: any) => `${d.data.nombre} (${d.data.dato})`)
+    .attr('transform', (d: any) => {
+      const a = (d.startAngle + d.endAngle) * 90 / Math.PI - 90;
+      const r = a > 90 ? a - 180 : a;
+      return `translate(${arc.centroid(d)}), rotate(${r})`;
+    }).style('text-anchor', 'middle');
+
   }
 
   private svgBarChart(selection: any, data: Array<any>) {
@@ -45,11 +73,12 @@ export class BarChartComponent implements OnInit {
       .append('rect').attr('x', rectanguloX).attr('y', rectanguloY).attr('width', 20).attr('height', rectanguloH)
       .style('fill', 'SteelBlue');
     svg.selectAll('text').data(data).enter().append('text').text(rectanguloH).attr('x', textoX).attr('y', textoY)
-    .style('font-size', '10px').style('text-anchor', 'middle');
+      .style('font-size', '10px').style('text-anchor', 'middle');
   }
 
-  private cargarJSON( path: string) {
-    const leer = (data: any) => console.log(data);
+  // tslint:disable-next-line: ban-types
+  private cargarJSON(path: string, selection: any, grafica: Function, sizeWindow: { h: any, w: any }) {
+    const leer = (data: any) => grafica(selection, data, sizeWindow);
     d3.json(path).then(leer);
   }
 
